9db44b110472a7ad4328cd9e8142ae7413ae2e09
Lennart Krellenberg <lennart@lennartkrellenberg.com>
2021-01-03 21:00:15 +0100

    added image modal popup

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,11 +1,17 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 const navbarBG = document.getElementsByClassName('navbar')[0]
+const images = document.querySelectorAll(".gallery-item");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const modalTxt = document.querySelector(".modalTxt");
+const close = document.querySelector(".close");
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
     navbarBG.classList.toggle('navbar-black')
     toggleButton.classList.toggle('clear-button')
+
 
 })
 
@@ -21,6 +27,20 @@ window.addEventListener('load', () => {
     const preload = document.querySelector('.preload');
     preload.classList.add('preload-finish')
 })
+
+
+
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        modalImg.src = image.src;
+        modalTxt.innerHTML = image.alt;
+        modal.classList.add("appear");
+
+        close.addEventListener("click", () => {
+            modal.classList.remove("appear");
+        });
+    });
+});
 
 
 
